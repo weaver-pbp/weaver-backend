643bb69ee93403e70902a4acf932f2b5eaf0904e
William Gooch <william.gooch.wg@gmail.com>
2020-10-11 19:22:48 +0100

    Add swagger api docs.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "dotenv";
 
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import * as session from "express-session";
 import * as passport from "passport";
 import { SESSION_SECRET } from "utils/secrets";
@@ -19,6 +20,14 @@ async function bootstrap() {
 
     app.use(passport.initialize());
     app.use(passport.session());
+
+    const options = new DocumentBuilder()
+        .setTitle("Weaver API")
+        .setDescription("The API documentation for the Weaver RPG project.")
+        .setVersion(process.env.npm_package_version)
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup("api", app, document);
 
     await app.listen(3000);
 }
