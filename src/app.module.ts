9981ffb7582eaa469343cd67e6439ca720dc4bf4
William Gooch <william.gooch.wg@gmail.com>
2020-10-11 14:28:46 +0100

    Add typeorm connection.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { UserService } from "./user/user.service";
 
 @Module({
-    imports: [],
+    imports: [TypeOrmModule.forRoot()],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [AppService, UserService],
 })
 export class AppModule {}
