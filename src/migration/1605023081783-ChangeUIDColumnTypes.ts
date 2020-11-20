2b5f503ef82aba4e161d2d7baef10ea90c7ae482
William Gooch <william.gooch.wg@gmail.com>
2020-11-20 14:37:34 +0000

    Fix weird stuff with migrations.

--- src/migration/1605023081783-ChangeUIDColumnTypes.ts
+++ src/migration/1605023081783-ChangeUIDColumnTypes.ts
@@ -1,76 +1,147 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class ChangeUIDColumnTypes1605023081783 implements MigrationInterface {
-    name = 'ChangeUIDColumnTypes1605023081783'
+    name = "ChangeUIDColumnTypes1605023081783";
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `gm` DROP FOREIGN KEY `FK_f51bbc19c2dda02faa52048c052`");
-        await queryRunner.query("ALTER TABLE `gm` DROP FOREIGN KEY `FK_6777b74ecd80d7a8ec70daec56e`");
+        await queryRunner.query(
+            "ALTER TABLE `gm` DROP FOREIGN KEY `FK_f51bbc19c2dda02faa52048c052`"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `gm` DROP FOREIGN KEY `FK_6777b74ecd80d7a8ec70daec56e`"
+        );
         await queryRunner.query("ALTER TABLE `gm` DROP PRIMARY KEY");
         await queryRunner.query("ALTER TABLE `gm` ADD PRIMARY KEY (`user_id`)");
         await queryRunner.query("ALTER TABLE `gm` DROP COLUMN `game_id`");
-        await queryRunner.query("ALTER TABLE `gm` ADD `game_id` binary(16) NOT NULL");
+        await queryRunner.query(
+            "ALTER TABLE `gm` ADD `game_id` binary(16) NOT NULL"
+        );
         await queryRunner.query("ALTER TABLE `gm` DROP PRIMARY KEY");
-        await queryRunner.query("ALTER TABLE `gm` ADD PRIMARY KEY (`user_id`, `game_id`)");
+        await queryRunner.query(
+            "ALTER TABLE `gm` ADD PRIMARY KEY (`user_id`, `game_id`)"
+        );
         await queryRunner.query("ALTER TABLE `gm` DROP PRIMARY KEY");
         await queryRunner.query("ALTER TABLE `gm` ADD PRIMARY KEY (`game_id`)");
         await queryRunner.query("ALTER TABLE `gm` DROP COLUMN `user_id`");
-        await queryRunner.query("ALTER TABLE `gm` ADD `user_id` binary(16) NOT NULL");
+        await queryRunner.query(
+            "ALTER TABLE `gm` ADD `user_id` binary(16) NOT NULL"
+        );
         await queryRunner.query("ALTER TABLE `gm` DROP PRIMARY KEY");
-        await queryRunner.query("ALTER TABLE `gm` ADD PRIMARY KEY (`game_id`, `user_id`)");
-        await queryRunner.query("ALTER TABLE `player` DROP FOREIGN KEY `FK_433f544c592c2b6cbdfd2edbec3`");
-        await queryRunner.query("ALTER TABLE `player` DROP FOREIGN KEY `FK_d04e64fc9b7fd372000c0dfda3f`");
+        await queryRunner.query(
+            "ALTER TABLE `gm` ADD PRIMARY KEY (`game_id`, `user_id`)"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `player` DROP FOREIGN KEY `FK_433f544c592c2b6cbdfd2edbec3`"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `player` DROP FOREIGN KEY `FK_d04e64fc9b7fd372000c0dfda3f`"
+        );
         await queryRunner.query("ALTER TABLE `player` DROP PRIMARY KEY");
-        await queryRunner.query("ALTER TABLE `player` ADD PRIMARY KEY (`user_id`)");
+        await queryRunner.query(
+            "ALTER TABLE `player` ADD PRIMARY KEY (`user_id`)"
+        );
         await queryRunner.query("ALTER TABLE `player` DROP COLUMN `game_id`");
-        await queryRunner.query("ALTER TABLE `player` ADD `game_id` binary(16) NOT NULL");
+        await queryRunner.query(
+            "ALTER TABLE `player` ADD `game_id` binary(16) NOT NULL"
+        );
         await queryRunner.query("ALTER TABLE `player` DROP PRIMARY KEY");
-        await queryRunner.query("ALTER TABLE `player` ADD PRIMARY KEY (`user_id`, `game_id`)");
+        await queryRunner.query(
+            "ALTER TABLE `player` ADD PRIMARY KEY (`user_id`, `game_id`)"
+        );
         await queryRunner.query("ALTER TABLE `player` DROP PRIMARY KEY");
-        await queryRunner.query("ALTER TABLE `player` ADD PRIMARY KEY (`game_id`)");
+        await queryRunner.query(
+            "ALTER TABLE `player` ADD PRIMARY KEY (`game_id`)"
+        );
         await queryRunner.query("ALTER TABLE `player` DROP COLUMN `user_id`");
-        await queryRunner.query("ALTER TABLE `player` ADD `user_id` binary(16) NOT NULL");
+        await queryRunner.query(
+            "ALTER TABLE `player` ADD `user_id` binary(16) NOT NULL"
+        );
         await queryRunner.query("ALTER TABLE `player` DROP PRIMARY KEY");
-        await queryRunner.query("ALTER TABLE `player` ADD PRIMARY KEY (`game_id`, `user_id`)");
-        await queryRunner.query("ALTER TABLE `gm` ADD CONSTRAINT `FK_f51bbc19c2dda02faa52048c052` FOREIGN KEY (`game_id`) REFERENCES `game`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION");
-        await queryRunner.query("ALTER TABLE `gm` ADD CONSTRAINT `FK_6777b74ecd80d7a8ec70daec56e` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION");
-        await queryRunner.query("ALTER TABLE `player` ADD CONSTRAINT `FK_433f544c592c2b6cbdfd2edbec3` FOREIGN KEY (`game_id`) REFERENCES `game`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION");
-        await queryRunner.query("ALTER TABLE `player` ADD CONSTRAINT `FK_d04e64fc9b7fd372000c0dfda3f` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION");
+        await queryRunner.query(
+            "ALTER TABLE `player` ADD PRIMARY KEY (`game_id`, `user_id`)"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `gm` ADD CONSTRAINT `FK_f51bbc19c2dda02faa52048c052` FOREIGN KEY (`game_id`) REFERENCES `game`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `gm` ADD CONSTRAINT `FK_6777b74ecd80d7a8ec70daec56e` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `player` ADD CONSTRAINT `FK_433f544c592c2b6cbdfd2edbec3` FOREIGN KEY (`game_id`) REFERENCES `game`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `player` ADD CONSTRAINT `FK_d04e64fc9b7fd372000c0dfda3f` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION"
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `player` DROP FOREIGN KEY `FK_d04e64fc9b7fd372000c0dfda3f`");
-        await queryRunner.query("ALTER TABLE `player` DROP FOREIGN KEY `FK_433f544c592c2b6cbdfd2edbec3`");
-        await queryRunner.query("ALTER TABLE `gm` DROP FOREIGN KEY `FK_6777b74ecd80d7a8ec70daec56e`");
-        await queryRunner.query("ALTER TABLE `gm` DROP FOREIGN KEY `FK_f51bbc19c2dda02faa52048c052`");
+        await queryRunner.query(
+            "ALTER TABLE `player` DROP FOREIGN KEY `FK_d04e64fc9b7fd372000c0dfda3f`"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `player` DROP FOREIGN KEY `FK_433f544c592c2b6cbdfd2edbec3`"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `gm` DROP FOREIGN KEY `FK_6777b74ecd80d7a8ec70daec56e`"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `gm` DROP FOREIGN KEY `FK_f51bbc19c2dda02faa52048c052`"
+        );
         await queryRunner.query("ALTER TABLE `player` DROP PRIMARY KEY");
-        await queryRunner.query("ALTER TABLE `player` ADD PRIMARY KEY (`game_id`)");
+        await queryRunner.query(
+            "ALTER TABLE `player` ADD PRIMARY KEY (`game_id`)"
+        );
         await queryRunner.query("ALTER TABLE `player` DROP COLUMN `user_id`");
-        await queryRunner.query("ALTER TABLE `player` ADD `user_id` binary(1) NOT NULL");
+        await queryRunner.query(
+            "ALTER TABLE `player` ADD `user_id` binary(1) NOT NULL"
+        );
         await queryRunner.query("ALTER TABLE `player` DROP PRIMARY KEY");
-        await queryRunner.query("ALTER TABLE `player` ADD PRIMARY KEY (`user_id`, `game_id`)");
+        await queryRunner.query(
+            "ALTER TABLE `player` ADD PRIMARY KEY (`user_id`, `game_id`)"
+        );
         await queryRunner.query("ALTER TABLE `player` DROP PRIMARY KEY");
-        await queryRunner.query("ALTER TABLE `player` ADD PRIMARY KEY (`user_id`)");
+        await queryRunner.query(
+            "ALTER TABLE `player` ADD PRIMARY KEY (`user_id`)"
+        );
         await queryRunner.query("ALTER TABLE `player` DROP COLUMN `game_id`");
-        await queryRunner.query("ALTER TABLE `player` ADD `game_id` binary(1) NOT NULL");
+        await queryRunner.query(
+            "ALTER TABLE `player` ADD `game_id` binary(1) NOT NULL"
+        );
         await queryRunner.query("ALTER TABLE `player` DROP PRIMARY KEY");
-        await queryRunner.query("ALTER TABLE `player` ADD PRIMARY KEY (`game_id`, `user_id`)");
-        await queryRunner.query("ALTER TABLE `player` ADD CONSTRAINT `FK_d04e64fc9b7fd372000c0dfda3f` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION");
-        await queryRunner.query("ALTER TABLE `player` ADD CONSTRAINT `FK_433f544c592c2b6cbdfd2edbec3` FOREIGN KEY (`game_id`) REFERENCES `game`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION");
+        await queryRunner.query(
+            "ALTER TABLE `player` ADD PRIMARY KEY (`game_id`, `user_id`)"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `player` ADD CONSTRAINT `FK_d04e64fc9b7fd372000c0dfda3f` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `player` ADD CONSTRAINT `FK_433f544c592c2b6cbdfd2edbec3` FOREIGN KEY (`game_id`) REFERENCES `game`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION"
+        );
         await queryRunner.query("ALTER TABLE `gm` DROP PRIMARY KEY");
         await queryRunner.query("ALTER TABLE `gm` ADD PRIMARY KEY (`game_id`)");
         await queryRunner.query("ALTER TABLE `gm` DROP COLUMN `user_id`");
-        await queryRunner.query("ALTER TABLE `gm` ADD `user_id` binary(1) NOT NULL");
+        await queryRunner.query(
+            "ALTER TABLE `gm` ADD `user_id` binary(1) NOT NULL"
+        );
         await queryRunner.query("ALTER TABLE `gm` DROP PRIMARY KEY");
-        await queryRunner.query("ALTER TABLE `gm` ADD PRIMARY KEY (`user_id`, `game_id`)");
+        await queryRunner.query(
+            "ALTER TABLE `gm` ADD PRIMARY KEY (`user_id`, `game_id`)"
+        );
         await queryRunner.query("ALTER TABLE `gm` DROP PRIMARY KEY");
         await queryRunner.query("ALTER TABLE `gm` ADD PRIMARY KEY (`user_id`)");
         await queryRunner.query("ALTER TABLE `gm` DROP COLUMN `game_id`");
-        await queryRunner.query("ALTER TABLE `gm` ADD `game_id` binary(1) NOT NULL");
+        await queryRunner.query(
+            "ALTER TABLE `gm` ADD `game_id` binary(1) NOT NULL"
+        );
         await queryRunner.query("ALTER TABLE `gm` DROP PRIMARY KEY");
-        await queryRunner.query("ALTER TABLE `gm` ADD PRIMARY KEY (`game_id`, `user_id`)");
-        await queryRunner.query("ALTER TABLE `gm` ADD CONSTRAINT `FK_6777b74ecd80d7a8ec70daec56e` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION");
-        await queryRunner.query("ALTER TABLE `gm` ADD CONSTRAINT `FK_f51bbc19c2dda02faa52048c052` FOREIGN KEY (`game_id`) REFERENCES `game`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION");
+        await queryRunner.query(
+            "ALTER TABLE `gm` ADD PRIMARY KEY (`game_id`, `user_id`)"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `gm` ADD CONSTRAINT `FK_6777b74ecd80d7a8ec70daec56e` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `gm` ADD CONSTRAINT `FK_f51bbc19c2dda02faa52048c052` FOREIGN KEY (`game_id`) REFERENCES `game`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION"
+        );
     }
-
 }
--- src/migration/1605023282639-ChangeOwnerColumnType.ts
+++ src/migration/1605023282639-ChangeOwnerColumnType.ts
@@ -1,20 +1,35 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class ChangeOwnerColumnType1605023282639 implements MigrationInterface {
-    name = 'ChangeOwnerColumnType1605023282639'
+    name = "ChangeOwnerColumnType1605023282639";
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `game` DROP FOREIGN KEY `FK_d05575b5a28ec6dad65c2aef301`");
-        await queryRunner.query("ALTER TABLE `game` CHANGE `ownerId` `owner_id` binary(16) NULL");
-        await queryRunner.query("ALTER TABLE `game` CHANGE `owner_id` `owner_id` binary(16) NOT NULL");
-        await queryRunner.query("ALTER TABLE `game` ADD CONSTRAINT `FK_678fcc30dbaf1c4c7e86bc10d16` FOREIGN KEY (`owner_id`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION");
+        await queryRunner.query(
+            "ALTER TABLE `game` DROP FOREIGN KEY `FK_d05575b5a28ec6dad65c2aef301`"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `game` CHANGE `ownerId` `owner_id` binary(16) NULL"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `game` CHANGE `owner_id` `owner_id` binary(16) NOT NULL"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `game` ADD CONSTRAINT `FK_678fcc30dbaf1c4c7e86bc10d16` FOREIGN KEY (`owner_id`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION"
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `game` DROP FOREIGN KEY `FK_678fcc30dbaf1c4c7e86bc10d16`");
-        await queryRunner.query("ALTER TABLE `game` CHANGE `owner_id` `owner_id` binary(16) NULL");
-        await queryRunner.query("ALTER TABLE `game` CHANGE `owner_id` `ownerId` binary(16) NULL");
-        await queryRunner.query("ALTER TABLE `game` ADD CONSTRAINT `FK_d05575b5a28ec6dad65c2aef301` FOREIGN KEY (`ownerId`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION");
+        await queryRunner.query(
+            "ALTER TABLE `game` DROP FOREIGN KEY `FK_678fcc30dbaf1c4c7e86bc10d16`"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `game` CHANGE `owner_id` `owner_id` binary(16) NULL"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `game` CHANGE `owner_id` `ownerId` binary(16) NULL"
+        );
+        await queryRunner.query(
+            "ALTER TABLE `game` ADD CONSTRAINT `FK_d05575b5a28ec6dad65c2aef301` FOREIGN KEY (`ownerId`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION"
+        );
     }
-
 }
